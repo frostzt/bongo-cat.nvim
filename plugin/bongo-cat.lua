bad1da512b44b7bb697dc1663f523126109a1890
frostzt <aidenfrostbite@gmail.com>
2026-01-03 18:12:14 +0530

    fix: update window

--- plugin/bongo-cat.lua
+++ plugin/bongo-cat.lua
@@ -3,49 +3,49 @@
 
 -- Prevent loading twice
 if vim.g.loaded_bongo_cat then
-  return
+	return
 end
 vim.g.loaded_bongo_cat = true
 
 -- Create user commands
 vim.api.nvim_create_user_command("BongoCat", function(opts)
-  local bongo = require("bongo-cat")
-  local arg = opts.args
+	local bongo = require("bongo-cat")
+	local arg = opts.args
 
-  if arg == "" or arg == "toggle" then
-    bongo.toggle()
-  elseif arg == "show" then
-    bongo.show()
-  elseif arg == "hide" then
-    bongo.hide()
-  elseif arg == "stats" then
-    bongo.show_stats()
-  elseif arg == "reset" then
-    bongo.reset_stats()
-  elseif arg == "health" then
-    local health = bongo.health()
-    vim.print(health)
-  else
-    -- Try to play animation by name
-    bongo.play(arg)
-  end
+	if arg == "" or arg == "toggle" then
+		bongo.toggle()
+	elseif arg == "show" then
+		bongo.show()
+	elseif arg == "hide" then
+		bongo.hide()
+	elseif arg == "stats" then
+		bongo.show_stats()
+	elseif arg == "reset" then
+		bongo.reset_stats()
+	elseif arg == "health" then
+		local health = bongo.health()
+		vim.print(health)
+	else
+		-- Try to play animation by name
+		bongo.play(arg)
+	end
 end, {
-  nargs = "?",
-  complete = function()
-    return { "toggle", "show", "hide", "stats", "reset", "health", "happy", "sad", "idle" }
-  end,
-  desc = "Control Bongo Cat",
+	nargs = "?",
+	complete = function()
+		return { "toggle", "show", "hide", "stats", "reset", "health", "happy", "sad", "idle" }
+	end,
+	desc = "Control Bongo Cat",
 })
 
 -- Convenience commands
 vim.api.nvim_create_user_command("BongoCatShow", function()
-  require("bongo-cat").show()
+	require("bongo-cat").show()
 end, { desc = "Show Bongo Cat" })
 
 vim.api.nvim_create_user_command("BongoCatHide", function()
-  require("bongo-cat").hide()
+	require("bongo-cat").hide()
 end, { desc = "Hide Bongo Cat" })
 
 vim.api.nvim_create_user_command("BongoCatStats", function()
-  require("bongo-cat").show_stats()
+	require("bongo-cat").show_stats()
 end, { desc = "Show Bongo Cat Stats" })
